87bfc089cfe23bbb38dfe80e80c51dbbf8803b76
0x10ud1ad <91825356+tayuprosper@users.noreply.github.com>
2025-07-23 20:32:12 +0100

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Poppins } from "next/font/google";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import Navabar from "./navbar";
 import Footer from "./(components)/footer";
